49623573bc0c83c139c747def0ec6ae5c2b2e2e9
Opilane TTHK <opilane@tthk.ee>
2020-11-17 11:19:49 +0200

    Add project files.

---
+++ Karusel/Karusel/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Karusel
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+           var redContedPage = new  ContentPage //nimetus
+           {
+               Content = new StackLayout
+               {
+                   Children =
+                   {
+                       new Label
+                       {
+                           Text = "Red",//tekst
+                           FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                           HorizontalOptions = LayoutOptions.Center
+                       },
+                       new BoxView
+                       { Color = Color.Red,//värv
+                        WidthRequest = 200,
+                        HeightRequest = 200,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                       }
+                   }
+               }
+           }
+        }
+    }
+}
